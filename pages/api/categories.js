d12cb71650c1ee61c38f206ce8ee7a1b9322e926
DesarolloTecno <roboticaeinovacion@gmail.com>
2023-08-01 09:05:29 -0300

    arreglos

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -1,27 +1,15 @@
 import { mongooseConnect } from "@/lib/mongoose";
 import mongoose from 'mongoose';
 import Category from "@/models/Category";
-import { getServerSession } from "next-auth";
-import { isAdminRequest } from "./auth/[...nextauth]";
 
 export default async function handle(req,res){
     const {method} = req;
     await mongooseConnect();
-    await isAdminRequest(req,res);
 
 
     if (method === 'GET'){
         res.json(await Category.find().populate('parent'));
     }
-
-    // if (method === 'POST'){
-    //     const {name,parentCategory} = req.body;
-    //     const categoryDoc = await Category.create({
-    //         name,
-    //         parent:parentCategory,
-    //     });
-    //     res.json(categoryDoc);
-    // }
 
     if (method === 'POST'){
         const { name, parentCategory,properties} = req.body;
