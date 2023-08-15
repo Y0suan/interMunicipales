afeb7c2808fdf7734c02f90e5e01200fd4d6d200
DesarolloTecno <roboticaeinovacion@gmail.com>
2023-08-15 06:54:43 -0300

    mejoras 3.0

--- Component/CartEventosHome.js
+++ Component/CartEventosHome.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
 import Link from 'next/link';
-import { AiOutlineArrowRight } from 'react-icons/ai';
+import { IoFootballOutline } from 'react-icons/io5';
 
 const Cont = styled.div`
-box-sizing: border-box;
+  border:1px solid #1B1C20;
+  box-sizing: border-box;
   margin: 0;
   padding: 5px;
-  min-width: 270px;
+  min-width: 200px;
+  height:auto;
   display: flex;
-  /* box-shadow: 0px 2px 6px 2px rgba(0, 0, 0, 0.15), 0px 1px 2px 0px rgba(0, 0, 0, 0.30); */
+  flex-direction: column;
+  box-shadow: 0px 2px 6px 2px rgba(0, 0, 0, 0.15), 0px 1px 2px 0px rgba(0, 0, 0, 0.30);
   justify-content: center;
-  background-color: #545558;
-  color:white;
+  color:black;
   border-radius:16px;
   .cont1{
     width: 100%;
@@ -21,38 +23,49 @@ box-sizing: border-box;
 const Title = styled.div`
 width: 100%;
 display:flex;
-justify-content: space-evenly;
-font-size:18px;
+justify-content: space-around;
+font-size:14px;
 font-weight: 700;
 line-height: 10px;
+font-family: 'Roboto', sans-serif;
+`;
+const Icon = styled.div`
+display: flex;
+align-items: center;
+border-radius: 50%;
+width: 100%;
+height: 50px;
+.icon{
+  font-size: 2rem;
+  color:black;
+}
 `;
 const Contenido = styled.div`
-width: 100%;
-display:flex;
-justify-content: space-evenly;
-font-size:14px;
-font-weight: 400;
+display: flex;
 font-family: 'Roboto', sans-serif;
-
+p{
+  font-size: 10px;
+}
 `;
 
 const CartEventosHome = ({ evet }) => {
   return (
     <Cont>
-        <div className='cont1'>
-            <Title className='header'>
+      <Icon>
+      <IoFootballOutline className='icon'></IoFootballOutline>
+      <Title className='header'>
             <p className='A' >{evet.equipoA}</p>
             <p>vs</p>
             <p className='B' >{evet.equipoB}</p>
-            </Title>
-        <Contenido>
+      </Title>
+      </Icon>
+      <Contenido>
         <p>{evet.hubicacion}</p>
         <p>/</p>
         <p>{evet.hora}</p>
         <p>/</p>
         <p>{evet.fecha}</p>
-        </Contenido>
-        </div>
+      </Contenido>
     </Cont>
   )
 }
